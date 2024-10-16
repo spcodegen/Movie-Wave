89efac15aacc5743e3b7fd9dff4d7bd29d52ecc7
spcodegen <shehansp2000@gmail.com>
2024-10-16 13:02:42 +0530

    Search movies with a Query

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:moviewave/models/movies_model.dart';
+import 'package:moviewave/service/movies_service.dart';
+import 'package:moviewave/widgets/search_detail.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,11 +11,127 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Movie> _searchresults = [];
+  bool _isLoading = false;
+  String _error = "";
+
+  //Method to search for movies
+  Future<void> _searchMovies() async {
+    setState(() {
+      _isLoading = true;
+      _error = "";
+    });
+
+    try {
+      print(_searchController.text);
+      List<Movie> movies =
+          await MoviesService().searchMovies(_searchController.text);
+
+      setState(() {
+        _searchresults = movies;
+      });
+    } catch (error) {
+      print("Error:$error");
+      setState(() {
+        _error = "Faild to search that movie";
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Search Movies"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        hintText: "Search for a movie",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                        ),
+                      ),
+                      onSubmitted: (_) => _searchMovies(),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.red[600],
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: _searchMovies,
+                      icon: const Icon(
+                        Icons.search,
+                        size: 30,
+                        weight: 10,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            if (_isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              )
+            else if (_error.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(
+                  _error,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              )
+            else if (_searchresults.isEmpty)
+              const Center(
+                child: Text("No movies found.please Search..."),
+              )
+            else
+              Expanded(
+                  child: ListView.builder(
+                itemCount: _searchresults.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      SearchWidget(
+                        movie: _searchresults[index],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Divider(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  );
+                },
+              ))
+          ],
+        ),
       ),
     );
   }
--- lib/service/movies_service.dart
+++ lib/service/movies_service.dart
@@ -8,15 +8,14 @@ import 'package:moviewave/models/movies_model.dart';
 class MoviesService {
   //get the api key from .env
   final String _apiKey = dotenv.env["TMDB_DATABASE_KEY"] ?? "";
-  final String _baseUrl = "https://api.themoviedb.org/3/movie/upcoming";
-  final String _baseUrlNew = "https://api.themoviedb.org/3/movie/now_playing";
+  final String _baseUrl = "https://api.themoviedb.org/3/movie";
 
   //Fetch all Upcomming Movies
   Future<List<Movie>> fetchUpcommingMovies({int page = 1}) async {
     try {
       final responce = await http.get(
         Uri.parse(
-          "$_baseUrl?api_key=$_apiKey&page=$page",
+          "$_baseUrl/upcoming?api_key=$_apiKey&page=$page",
         ),
       );
 
@@ -37,8 +36,8 @@ class MoviesService {
   //fetch all now playing movies
   Future<List<Movie>> fetchNowPlayingMovies({int page = 1}) async {
     try {
-      final responce =
-          await http.get(Uri.parse("$_baseUrlNew?api_key=$_apiKey&page=$page"));
+      final responce = await http
+          .get(Uri.parse("$_baseUrl/now_playing?api_key=$_apiKey&page=$page"));
 
       if (responce.statusCode == 200) {
         final data = json.decode(responce.body);
@@ -51,6 +50,28 @@ class MoviesService {
     } catch (error) {
       print("Error fetching now playing movies $error");
       return [];
+    }
+  }
+
+  //Search movie by query
+  //https://api.themoviedb.org/3/search/movie?query=ice age&api_key=a8d4032665d01f0c0b9a8a5ab7e61ccc
+  Future<List<Movie>> searchMovies(String query) async {
+    try {
+      final responce = await http.get(
+        Uri.parse(
+            "https://api.themoviedb.org/3/search/movie?query=$query&api_key=$_apiKey"),
+      );
+      if (responce.statusCode == 200) {
+        final data = json.decode(responce.body);
+        final List<dynamic> results = data["results"];
+
+        return results.map((movieData) => Movie.fromJson(movieData)).toList();
+      } else {
+        throw Exception("Error Searching movies");
+      }
+    } catch (error) {
+      print("Error searchin movie:$error");
+      throw Exception("Error searchin movie:$error");
     }
   }
 }
