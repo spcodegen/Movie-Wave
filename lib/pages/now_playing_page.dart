0bea5f2573548bc3091c0848969f57bf1ca58b2e
spcodegen <shehansp2000@gmail.com>
2024-10-16 11:04:21 +0530

    Search movies with a Query

--- lib/pages/now_playing_page.dart
+++ lib/pages/now_playing_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:moviewave/models/movies_model.dart';
+import 'package:moviewave/service/movies_service.dart';
+import 'package:moviewave/widgets/movie_details.dart';
 
 class NowPlayingPage extends StatefulWidget {
   const NowPlayingPage({super.key});
@@ -8,12 +11,108 @@ class NowPlayingPage extends StatefulWidget {
 }
 
 class _NowPlayingPageState extends State<NowPlayingPage> {
+  List<Movie> _movies = [];
+  int _currentPage = 1;
+  int _totalPages = 1;
+  bool _isLoading = false;
+
+  //Method to fetch the movies
+  Future<void> _fetchMovies() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      List<Movie> fetchedMovies =
+          await MoviesService().fetchNowPlayingMovies(page: _currentPage);
+
+      setState(() {
+        _movies = fetchedMovies;
+        _totalPages = 100;
+      });
+    } catch (error) {
+      print("Error:$error");
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  //go to previous page
+  void _previousPage() {
+    if (_currentPage > 1) {
+      setState(() {
+        _currentPage--;
+      });
+      _fetchMovies();
+    }
+  }
+
+  //go to next page
+  void _nextPage() {
+    if (_currentPage < _totalPages) {
+      setState(() {
+        _currentPage++;
+      });
+      _fetchMovies();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMovies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Now Playing Movies"),
+        title: const Text("Now Playing Movies"),
       ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: _movies.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index > _movies.length - 1) {
+                      return _buildPaginationControls();
+                    } else {
+                      return MovieDetailsWidget(movie: _movies[index]);
+                    }
+                  },
+                ))
+              ],
+            ),
+    );
+  }
+
+  Widget _buildPaginationControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: _currentPage > 1 ? _previousPage : null,
+          child: const Text("Previous"),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Text('Page $_currentPage of $_totalPages'),
+        const SizedBox(
+          width: 8,
+        ),
+        ElevatedButton(
+          onPressed: _currentPage < _totalPages ? _nextPage : null,
+          child: const Text("Next"),
+        ),
+      ],
     );
   }
 }
--- lib/service/movies_service.dart
+++ lib/service/movies_service.dart
@@ -9,6 +9,7 @@ class MoviesService {
   //get the api key from .env
   final String _apiKey = dotenv.env["TMDB_DATABASE_KEY"] ?? "";
   final String _baseUrl = "https://api.themoviedb.org/3/movie/upcoming";
+  final String _baseUrlNew = "https://api.themoviedb.org/3/movie/now_playing";
 
   //Fetch all Upcomming Movies
   Future<List<Movie>> fetchUpcommingMovies({int page = 1}) async {
@@ -29,6 +30,26 @@ class MoviesService {
       }
     } catch (error) {
       print("Error fetching upcoming movies $error");
+      return [];
+    }
+  }
+
+  //fetch all now playing movies
+  Future<List<Movie>> fetchNowPlayingMovies({int page = 1}) async {
+    try {
+      final responce =
+          await http.get(Uri.parse("$_baseUrlNew?api_key=$_apiKey&page=$page"));
+
+      if (responce.statusCode == 200) {
+        final data = json.decode(responce.body);
+        final List<dynamic> results = data["results"];
+
+        return results.map((movieData) => Movie.fromJson(movieData)).toList();
+      } else {
+        throw Exception("Faild to load the data");
+      }
+    } catch (error) {
+      print("Error fetching now playing movies $error");
       return [];
     }
   }
